6ad812318076ddb4148d801dee671b024a9e5e4e
Cyrus <mdoaie@yahoo.co.uk>
2019-07-09 11:59:29 +0100

    preparing

--- VPNStatusTray/Properties/AssemblyInfo.cs
+++ VPNStatusTray/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SystemTrayApp")]
+[assembly: AssemblyTitle("VPNStatusTray")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SystemTrayApp")]
+[assembly: AssemblyProduct("VPNStatusTray")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
